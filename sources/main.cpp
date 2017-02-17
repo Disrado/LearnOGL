81fd92a9412d12ca4ce5f1bae721268eee33654c
Disrado <n.maltsev.dis@gmail.com>
2017-02-17 11:59:59 +0200

    Rectangle rendering

--- sources/main.cpp
+++ sources/main.cpp
@@ -66,7 +66,7 @@ int main()
 	glCompileShader(vertexShader);
 
 //	GLint succes;
-//	GLchar infolog[128];
+//	GLchar infolog[512];
 //	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &succes);
 //	if (!succes) {
 //	 	glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
@@ -108,6 +108,8 @@ int main()
 	
 //----	Set up vertex and buffers data and attribute pointers  ----//
 
+/*
+	----Triangle----
 	GLfloat vertices[] = {
 		-0.5f, -0.5f, 0.0f, //Left
 		 0.5f, -0.5f, 0.0f, //Right
@@ -127,7 +129,43 @@ int main()
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-	
+*/
+
+	//----Rectangle----
+	GLfloat vertices[] = {
+		 0.5f,  0.5f, 0.0f, //Top right
+		 0.5f, -0.5f, 0.0f, //Bottom right
+		-0.5f, -0.5f, 0.0f, //Bottom left
+		-0.5f,  0.5f, 0.0f  //Top left
+	};
+
+	GLuint indices[] = { //Start from 0!
+		0, 1, 3, //First triangle
+		1, 2, 3  //Second triangle
+	};
+
+	GLuint VAO, VBO, IBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &IBO);
+
+	//Bind the  vertex array object first and set vertex buffers data and attribute pointers
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);	
+
+
+
 //----Game Loop----//	
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -137,17 +175,19 @@ int main()
 
 		//Draw triangle
 		glUseProgram(shaderProgram);
-   		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3); //for triangle 
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //for rectangle
 		glBindVertexArray(0);
 
 		//Swaps buffers
 		glfwSwapBuffers(window);
 	}
-
+	
 	//Properly de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &IBO);
 	
 	//clearing any resources allocated by GLFW 
 	glfwTerminate();	
